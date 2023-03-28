374fe78d6994bae44485cf7f3e4134fc9e893202
yuki3738 <y.minamiya@gmail.com>
2023-03-29 02:33:23 +0900

    bookクエリを作る
    https://zenn.dev/lilac/books/37bdf5d90b5f9b/viewer/af31f9#book%E3%82%AF%E3%82%A8%E3%83%AA%E3%82%92%E4%BD%9C%E3%82%8B

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,6 +8,7 @@ module Types
     # They will be entry points for queries on your schema.
 
     field :books, resolver: Queries::Books
+    field :book, resolver: Queries::Book
     # TODO: remove me
     # field :test_field, String, null: false,
     #   description: "An example field added by the generator"
