371140e5f31083a6e80ba491ff2879afc064d16e
yuki3738 <y.minamiya@gmail.com>
2023-05-01 00:31:10 +0900

    Apollo Clientを導入する
    https://zenn.dev/lilac/books/37bdf5d90b5f9b/viewer/90ef78

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    createHttpLink,
+  } from "@apollo/client";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const link = createHttpLink({
+    uri: "http://localhost:3000/graphql",
+    credentials: "include",
+  });
+
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: link,
+  });
+
+ReactDOM.render(
+    <React.StrictMode>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
